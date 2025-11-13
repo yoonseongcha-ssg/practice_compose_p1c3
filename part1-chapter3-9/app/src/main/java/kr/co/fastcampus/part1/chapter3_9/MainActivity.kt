57a88e942e3675a40acbc7d57c654519528f83c4
차윤성 <yoonseongcha@ssg.com>
2025-11-13 15:58:56 +0900

    P1 C3 09. BoxWithConstraints

--- part1-chapter3-9/app/src/main/java/kr/co/fastcampus/part1/chapter3_9/MainActivity.kt
+++ part1-chapter3-9/app/src/main/java/kr/co/fastcampus/part1/chapter3_9/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.co.fastcampus.part1.chapter3_9.ui.theme.BoxWithConstraintsTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,20 +29,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Outer() {
     // 스텝 4: Column에 width를 지정해서 제한해 봅시다.
-    Column {
+    Column(modifier = Modifier.width(150.dp)) {
         // 스텝 2: Inner의 인자로 Modifier.widthIn(min = 100.dp)를
         // 전달해봅시다. heightIn도 전달해봅시다. 각각 인자의 max값도
         // 전달해봅시다.
-        Inner()
+        Inner(
+            modifier = Modifier
+                .width(200.dp)
+                .height(160.dp)
+        )
+        Inner(
+            modifier = Modifier
+                .width(200.dp)
+                .height(100.dp)
+        )
     }
 }
 
 // 스텝 1: Inner 인자로 modifier를 전달해봅시다. 기본 값을 Modifier로 지정합시다.
 // 파라미터로 받은 modifier를 BoxWithConstrains에 전달합시다.
 @Composable
-private fun Inner() {
-    BoxWithConstraints {
+private fun Inner(modifier: Modifier = Modifier) {
+    BoxWithConstraints(modifier) {
         // 스텝 3: maxHeight 값이 150dp가 넘을 때만 추가로 텍스트를 출력해봅시다.
+        if (maxHeight > 150.dp) {
+            Text(
+                text = "여기 꽤 길군요!",
+                modifier = Modifier.align(Alignment.BottomCenter)
+            )
+        }
         Text("maxW:$maxWidth maxH:$maxHeight minW: $minWidth minH:$minHeight")
     }
 }
